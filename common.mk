657811b225b395896697ec8b08149935566d8d62
Martin J. Pollard <martin0566@gmail.com>
2025-09-12 01:30:28 -0400

    Revert "sm8350-common: Move few kernel scripts to device tree"
    This reverts commit 81b9c7e288a2c25493ee1bdd930a60dd60b980d0.

--- common.mk
+++ common.mk
@@ -178,8 +178,6 @@ PRODUCT_PACKAGES += \
     fstab.default \
     fstab.default.vendor_ramdisk \
     init.class_main.sh \
-    init.kernel.post_boot-lahaina.sh \
-    init.kernel.post_boot-yupik.sh \
     init.oplus.hw.rc \
     init.oplus.hw.rc.recovery \
     init.oplus.rc \
@@ -190,7 +188,6 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
-    init.qti.kernel.rc \
     init.target.rc \
     ueventd.oplus.rc \
     ueventd.qcom.rc
